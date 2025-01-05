b1e9519152ceec856e740a04598827cf62a82a68
bmspd <my_spid@mail.ru>
2025-01-05 11:44:31 +0100

    build(deps): eslint 9 configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Image from 'next/image'
 import animationSrc from '/public/animation.gif'
 import Logo from '/public/icons/logo.svg'
 export default function Home() {
-  const test = 'a'
-  console.log('123')
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <Image className="size-[300px]" src={animationSrc} alt="Main animation" unoptimized />
