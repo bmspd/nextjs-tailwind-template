7dd872f21e3e9d4aea8009d4e1f9664c8c4176b9
bmspd <my_spid@mail.ru>
2025-01-05 11:36:48 +0100

    feat(deps): add tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
-import styles from './page.module.css'
 import animationSrc from '/public/animation.gif'
 import Logo from '/public/icons/logo.svg'
 export default function Home() {
+  const test = 'a'
+  console.log('123')
   return (
-    <main className={styles.main}>
-      <Image className={styles.animation} src={animationSrc} alt="Main animation" unoptimized />
-      <h1 className={styles.text}>Template for Next.js</h1>
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <Image className="size-[300px]" src={animationSrc} alt="Main animation" unoptimized />
+      <h1 className="gradient-text">Template for Next.js</h1>
       <Logo width="300" height="auto" />
     </main>
   )
---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'tailwindcss'
+const config: Config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'nunito': 'var(--font-nunito)',
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
